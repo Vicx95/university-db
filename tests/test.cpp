afeed9e160fbaa3c04ebb607c85b37542a28e6fc
Vicx95 <wiktor.zyla31@gmail.com>
2023-06-04 12:38:28 +0200

    Refactor: Add DatabaseTest fixture

--- tests/test.cpp
+++ tests/test.cpp
@@ -1,16 +1,17 @@
 #include <gtest/gtest.h>
 #include "../src/Database.hpp"
 
-TEST(CheckStructure, CanAddStudentToDb) {
-    Student adam{"Adam", "Kowalski", "ul. Dobra 134, 00-200 Warszawa", 123456, "11223344567", Gender::Male};
-
+struct DatabaseTest : ::testing::Test {
     Database db;
+};
+
+TEST_F(DatabaseTest, CanAddStudentToDb) {
+    Student adam{"Adam", "Kowalski", "ul. Dobra 134, 00-200 Warszawa", 123456, "11223344567", Gender::Male};
     EXPECT_TRUE(db.add(adam));
     EXPECT_FALSE(db.add(adam));
 }
 
-TEST(DisplayDb, DisplayEmptyDb) {
-    Database db;
+TEST_F(DatabaseTest, DisplayEmptyDb) {
     auto content = db.show();
     auto expected = "";
     EXPECT_EQ(content, expected);
